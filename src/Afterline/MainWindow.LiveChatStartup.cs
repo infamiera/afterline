6454f770aee7bcf4a33fe8a833653b2742c2c68f
Infamiera <infamiera@gmail.com>
2026-08-21 15:18:48 +0200

    Initialize Live Chat enhancements after window load

---
+++ src/Afterline/MainWindow.LiveChatStartup.cs
@@ -0,0 +1,10 @@
+namespace Afterline;
+
+public partial class MainWindow
+{
+    protected override void OnContentRendered(EventArgs e)
+    {
+        base.OnContentRendered(e);
+        EnsureLiveChatEnhancements();
+    }
+}
